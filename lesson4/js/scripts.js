d3e8b5a364fe1a481a6a1d9a34c9fad407e877d8
G-code95 <gene.sunglao@gmail.com>
2021-05-22 13:48:52 +0800

    lesson 4 updates

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -2,7 +2,7 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
-const datefield = document.querySelector("date");
+const datefield = document.querySelector(".date");
 
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(now);
