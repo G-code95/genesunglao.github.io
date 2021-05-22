9435073134ebfefe4ca7fd4708abad6c4ef1695e
G-code95 <gene.sunglao@gmail.com>
2021-05-23 03:31:42 +0800

    lesson 5 Update

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -8,3 +8,19 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(now);
 
 datefield.textContent = fulldate;
+
+//banner//
+const today = new Date();
+console.log(today);
+
+const dayNumber = today.getDay();
+console.log(dayNumber);
+
+const element = document.getElementById("banner")
+
+if (dayNumber == 5) {
+    element.classList.add("showme");
+} 
+else {
+    element.classList.add("hideme");
+}
